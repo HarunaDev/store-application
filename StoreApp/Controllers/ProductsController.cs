2a61ab832bfbf0d307370f56b283c01ea58d37d1
Alvin Haruna <harunaalvin69@gmail.com>
2026-05-25 00:41:48 +0100

    modified product controller to allow for crud operations on the product data

--- StoreApp/Controllers/ProductsController.cs
+++ StoreApp/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
 using StoreApp.Services;
 
 namespace StoreApp.Controllers;
@@ -18,5 +19,62 @@ public class ProductsController : ControllerBase
     public IActionResult GetProducts()
     {
         return Ok(_productService.GetProducts());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProduct(int id)
+    {
+        var product = _productService.GetProductById(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public IActionResult AddProduct(Product product)
+    {
+        var createdProduct = _productService.AddProduct(product);
+
+        return CreatedAtAction(
+            nameof(GetProduct),
+            new { id = createdProduct.Id },
+            createdProduct
+        );
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateProduct(
+        int id,
+        Product updatedProduct
+    )
+    {
+        var updated = _productService.UpdateProduct(
+            id,
+            updatedProduct
+        );
+
+        if (!updated)
+        {
+            return NotFound();
+        }
+
+        return Ok(updatedProduct);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteProduct(int id)
+    {
+        var deleted = _productService.DeleteProduct(id);
+
+        if (!deleted)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
     }
 }
